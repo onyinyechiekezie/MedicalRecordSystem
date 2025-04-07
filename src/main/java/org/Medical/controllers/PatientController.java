4abb5386b6dc4eaa7ab9d1f71cdd1cf44508bd68
0nyinye <onyinyeekezi2017@gmail.com>
2025-04-07 14:30:38 +0100

    Added doctor login and register

--- src/main/java/org/Medical/controllers/PatientController.java
+++ src/main/java/org/Medical/controllers/PatientController.java
@@ -1,12 +1,13 @@
 package org.Medical.controllers;
 
+
 import org.Medical.data.models.Patient;
+import org.Medical.dto.request.patient.LoginPatientRequest;
 import org.Medical.dto.request.patient.RegisterPatientRequest;
 import org.Medical.services.PatientServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import static org.Medical.utility.PatientMapper.mapToPatient;
 
 @RestController
@@ -27,10 +28,17 @@ public class PatientController {
        }
     }
 
+
     @PostMapping("login")
-    public Patient login(@RequestBody Patient patient) {
-        return patientServices.loginPatient(patient.getEmail(), patient.getPassword());
+    public ResponseEntity<String> login(@RequestBody LoginPatientRequest request) {
+        try {
+            patientServices.loginPatient(request.getEmail(), request.getPassword());
+            return ResponseEntity.ok("Patient logged in successfully");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
+
 
 
 
